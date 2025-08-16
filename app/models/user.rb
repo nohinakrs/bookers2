1f95c9f4c2145537ed83806ceb344257d7542d61
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-08-16 11:18:54 +0000

    [add] 4-27 左カラム完成。メソッド名の見直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
          has_one_attached :profile_image
          has_many :books, dependent: :destroy
 
-  
-         def get_profile_image
+  #以下の表記は画像がない時にどうするかの処理をする記述、高さ幅の変数を冒頭で（）内に定義
+         def get_profile_image(width, height)
            unless profile_image.attached?
              file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
              profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
