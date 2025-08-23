662e78459e03ebb538ed14e57603cde4e17ae161
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-08-23 09:44:48 +0000

    [add] 4-27 投稿機能実装に向けて修正中2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,17 @@ class UsersController < ApplicationController
     @books = Book.all
     @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :books, only:[:index, :show, :create, :edit, :update, :destroy]
+
+  resources :books, only:[:edit, :create, :index, :show, :destroy]
   resources :users, only:[:index, :show, :edit, :update]
   
   get "homes/about" => "homes#about", as: "about"
