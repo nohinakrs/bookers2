b0e95a3fead694be3f10ea8ff90b6bdff9d90f35
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-09-02 09:54:18 +0000

    [add] 4-27 エラーメッセージを一部実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,6 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-     flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)
     else
       render "edit"
@@ -42,7 +41,6 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     if book.destroy
-      flash[:notice] = "Book was successfully destroyed."
       redirect_to '/books'
     end
   end
