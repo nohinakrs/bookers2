65c674c4e850247dc87441c03e1035346b68d733
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-09-04 07:31:19 +0000

    [add] 4-27 エラーメッセージの実装、アイコンの整列完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,14 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+     flash[:notice] = "Book was successfully created."
+     redirect_to user_path(@user.id)
+    else
+      render "edit"
+    end
   end
+
 
   private
 
