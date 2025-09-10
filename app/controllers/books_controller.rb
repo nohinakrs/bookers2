7eace4c4a6a79058306b6a552c01b7f36a9bc173
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-09-10 09:18:56 +0000

    [add] 4-27 全エラー修正済み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @newbook = Book.new
     @books = Book.all
@@ -54,5 +56,12 @@ class BooksController < ApplicationController
   
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
